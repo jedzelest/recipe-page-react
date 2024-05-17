abcf148a42782e31346013d8c1b85601e7d09f9f
jestrelloso <jestrelloso@jairosoftcom.onmicrosoft.com>
2024-05-17 14:30:05 +0800

    finished page ver1

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './components/Header'
-import UnorderedList from './components/UnorderedList'
-import OrderedList from './components/OrderedList'
-import { headlines } from './services/Headline'
-import { preparationList } from './services/PreparationHeadline'
-import { ingredientList } from './services/Ingredients'
-import { instructionList } from './services/Instructions'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./components/Header";
+import UnorderedList from "./components/UnorderedList";
+import OrderedList from "./components/OrderedList";
+import Nutrition from "./components/Nutrition";
+import { headlines } from "./services/Headline";
+import { preparationList } from "./services/PreparationHeadline";
+import { ingredientList } from "./services/Ingredients";
+import { instructionList } from "./services/Instructions";
+import { nutritionList } from "./services/Nutrition";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div className="container">
-        <Header headers={headlines}/>
+        <Header headers={headlines} />
         <section className="preparation">
-          <h4>Preparation time</h4>
-          <UnorderedList customClassName='bold' list={preparationList}/>
+          <h3>Preparation time</h3>
+          <UnorderedList customClassName="bold" list={preparationList} />
         </section>
         <section className="ingredients">
           <h3>Ingredients</h3>
-          <UnorderedList list={ingredientList}/>
+          <UnorderedList list={ingredientList} />
         </section>
-        <section className="Instructions">
+        <section className="instructions">
           <h3>Instructions</h3>
-          <OrderedList list={instructionList}/>
+          <OrderedList list={instructionList} />
+        </section>
+        <section className="nutrition">
+          <h3>Nutrition</h3>
+          <p>
+            The table below shows nutritional value per serving without the
+            additional fillings.
+          </p>
+          <Nutrition list={nutritionList} />
         </section>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/Nutrition.ts
@@ -0,0 +1,22 @@
+export const nutritionList = [
+  {
+    id: 1,
+    label: "Calories",
+    amount: "277kcal",
+  },
+  {
+    id: 2,
+    label: "Carbs",
+    amount: "0g",
+  },
+  {
+    id: 3,
+    label: "Protein",
+    amount: "20g",
+  },
+  {
+    id: 4,
+    label: "Fat",
+    amount: "22g",
+  },
+];
