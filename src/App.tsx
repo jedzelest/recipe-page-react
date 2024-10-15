6b2224a7728f2ae42694e9c81308e8055f2100c5
jedzelest <jedzelestrelloso@gmail.com>
2024-10-15 17:51:35 +0800

    moved the img to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Header from "./components/Header";
 import UnorderedList from "./components/UnorderedList";
 import OrderedList from "./components/OrderedList";
--- src/services/Headline.ts
+++ src/services/Headline.ts
@@ -1,7 +1,7 @@
 export const headlines = {
-    imageUrl: 'src/assets/img/image-omelette.jpeg',
-    headerTitle: 'Simple Omelette Recipe',
-    headerParagraph: `An easy and quick dish, perfect for any meal. This classic 
+  imageUrl: ".././public/img/image-omelette.jpeg",
+  headerTitle: "Simple Omelette Recipe",
+  headerParagraph: `An easy and quick dish, perfect for any meal. This classic 
     omelette combines beaten eggs cooked to perfection, optionally filled with your
-    choice of cheese, vegetables, or meats.`
-  }
+    choice of cheese, vegetables, or meats.`,
+};
