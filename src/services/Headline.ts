8be48747a6f468805f092f7e293b3635d4bfda98
jedzelest <jedzelestrelloso@gmail.com>
2024-10-15 18:03:24 +0800

    re-configured the img for public view

--- src/services/Headline.ts
+++ src/services/Headline.ts
@@ -1,5 +1,5 @@
 export const headlines = {
-  imageUrl: ".././public/img/image-omelette.jpeg",
+  imageUrl: "./img/image-omelette.jpeg",
   headerTitle: "Simple Omelette Recipe",
   headerParagraph: `An easy and quick dish, perfect for any meal. This classic 
     omelette combines beaten eggs cooked to perfection, optionally filled with your
