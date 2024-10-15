811937878e1a05596fcb3488242bf64e25e4c5c4
jedzelest <jedzelestrelloso@gmail.com>
2024-10-15 17:42:18 +0800

    re-configured Header Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,21 @@
 type HeaderProps = {
-    headers: {
-        headerTitle: string,
-        headerParagraph: string,
-        imageUrl: string,
-    }
-}
+  headers: {
+    headerTitle: string;
+    headerParagraph: string;
+    imageUrl: string;
+  };
+};
 
-const Header = ({headers: {headerTitle, headerParagraph, imageUrl}}: HeaderProps) => {
-    return (
-        <header className="heading">
-            <img src={imageUrl} alt="Image of an Omelette" />
-            <h1>{headerTitle}</h1>
-            <p>{headerParagraph}</p>
-        </header>
-    )
-}
+const Header = ({
+  headers: { headerTitle, headerParagraph, imageUrl },
+}: HeaderProps) => {
+  return (
+    <header className="heading">
+      <img src={imageUrl} alt="Image of an Omelette" />
+      <h1>{headerTitle}</h1>
+      <p>{headerParagraph}</p>
+    </header>
+  );
+};
 
 export default Header;
